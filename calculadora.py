07e0294618f8741321f953d8610d3e6ab3d63cd4
Gui Ryan <guilhermeryanzinho77@gmail.com>
2026-01-31 14:35:29 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,22 @@
+print("Digite o primeiro número:")
+a = float(input())
+
+print("Digite o segundo número:")
+b = float(input())
+
+print("Digite a operação (+  -  *  /):")
+op = input()
+
+print("Resultado:")
+
+if op == "+":
+    print(a + b)
+
+if op == "-":
+    print(a - b)
+
+if op == "*":
+    print(a * b)
+
+if op == "/":
+    print(a / b)
