66c1bde2581fadfe8cd424a09f97a7fe27d8df92
Gui Ryan <guilhermeryanzinho77@gmail.com>
2026-02-07 10:48:36 -0300

    Add files via upload

---
+++ Aula.py_7.py
@@ -0,0 +1,33 @@
+# Exercício 0
+for num in range(2, 21, 2):
+    print(num)
+
+print("-----")
+
+# Exercício 1
+numeros = list(range(1, 10))
+print(numeros)
+
+print("-----")
+
+# Exercício 2
+print(numeros[2])
+
+print("-----")
+
+# Exercício 3
+numeros.append(9)
+print(numeros)
+
+print("-----")
+
+# Exercício 4
+numeros.remove(5)
+print(numeros)
+
+print("-----")
+
+# Exercício 5
+carros = ["Gol", "Civic", "Fusca"]
+resultado = numeros + carros
+print(resultado)
