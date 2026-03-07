7e840cf463f712a2f6856ba7cfb1e72f34eea8fd
Gui Ryan <guilhermeryanzinho77@gmail.com>
2026-03-07 14:55:38 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+#main.py
+
+import meu_modulo 
+
+
+
+# aleatorio = meu_modulo.aleatorio(5,7)
+# print(aleatorio)
+
+# meu_modulo.py
+
+# 1 - Crie um número aleatório de 5,10
+
+
+
+# def atividade_1(n1,n2):
+#    return random.randint(n1, n2)
+
+print(meu_modulo.atividade2(10,30))
---
+++ meu_modulo.py
@@ -0,0 +1,3 @@
+import random 
+def atividade2(x,y):
+    return random.randrange(x,y)
